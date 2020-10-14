60a5f9a7047fecf658d5e0f504d44bb6fa730244
Taiany <braziltaiany@outlook.com>
2020-10-14 10:52:11 -0300

    Criando orphanages sem imagens

--- backend/src/database/migrations/1602676131581-create_orphanages.ts
+++ backend/src/database/migrations/1602676131581-create_orphanages.ts
@@ -40,6 +40,10 @@ export class createOrphanages1602640032330 implements MigrationInterface {
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false,
