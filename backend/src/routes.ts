010acb0bc4d9c2d24110fb0c2bbee5cd132dcca3
Taiany <braziltaiany@outlook.com>
2020-10-14 11:24:33 -0300

    listando orphanages

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import OrphanageController from './controllers/OrphanagesController';
 
 const routes = Router();
 
+routes.get('/orphanages', OrphanageController.index);
 routes.post('/orphanages', OrphanageController.create);
 
 export default routes;
