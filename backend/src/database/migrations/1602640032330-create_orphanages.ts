4764c8a02fa78777f619ca2a8de6144531214303
Taiany <braziltaiany@outlook.com>
2020-10-13 23:31:16 -0300

    criacao de tabelas no banco

---
+++ backend/src/database/migrations/1602640032330-create_orphanages.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createOrphanages1602640032330 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    //Realizar alterações (criar tabela,criar novo campo,deletar algum campo)
+    await queryRunner.createTable(
+      new Table({
+        name: 'orphanages',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'instructions',
+            type: 'text',
+          },
+          {
+            name: 'open_on_weekends',
+            type: 'boolean',
+            default: false,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    //desfazer alterações no método up
+    await queryRunner.dropTable('orphanages');
+  }
+}
