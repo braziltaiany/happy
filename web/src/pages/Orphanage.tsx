15c1eca928f0d660f0ea86637cf5cfab9f9c9362
Taiany <braziltaiany@outlook.com>
2020-10-16 14:13:07 -0300

    Detalhe do orphanage - imagens

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -31,6 +31,7 @@ interface OrphanageParams {
 export default function Orphanage() {
   const params = useParams<OrphanageParams>();
   const [orphanage, setOrphanage] = useState<Orphanage>();
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
 
   useEffect(() => {
     api.get(`/orphanages/${params.id}`).then((response) => {
@@ -49,12 +50,22 @@ export default function Orphanage() {
 
       <main>
         <div className="orphanage-details">
-          <img src={orphanage.images[0].url} alt={orphanage.name} />
+          <img
+            src={orphanage.images[activeImageIndex].url}
+            alt={orphanage.name}
+          />
 
           <div className="images">
-            {orphanage.images.map((image) => {
+            {orphanage.images.map((image, index) => {
               return (
-                <button key={image.id} className="active" type="button">
+                <button
+                  key={image.id}
+                  className={activeImageIndex === index ? 'active' : ''}
+                  type="button"
+                  onClick={() => {
+                    setActiveImageIndex(index);
+                  }}
+                >
                   <img src={image.url} alt={orphanage.name} />
                 </button>
               );
@@ -89,8 +100,8 @@ export default function Orphanage() {
               <footer>
                 <a
                   target="_blank"
-                  rel="nooopener "
-                  href={`https://www.google.com/maps/dir/?api=1&destintion=${orphanage.latitude},${orphanage.longitude}`}
+                  rel="noopener noreferrer"
+                  href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}
                 >
                   Ver rotas no Google Maps
                 </a>
